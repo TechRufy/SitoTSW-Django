568b58b15d906b3f44ddd16893f80527276a8776
TechRufy <alb.genovese@gmail.com>
2022-10-03 18:00:28 +0200

    robe

--- KindKaribe/models.py
+++ KindKaribe/models.py
@@ -1,8 +1,16 @@
-from django.db import models
-
+import enum
 # Create your models here.
 from django.db import models
 
 
-class utente(models.Model):
+class Genere():
+    Maschio = 'Maschio'
+    Femmina = 'Femmina'
+
+
+class Utente(models.Model):
     nome = models.CharField(max_length=200)
+    cognome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    nTelefono = models.CharField(max_length=200)
